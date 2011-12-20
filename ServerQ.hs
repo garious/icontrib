e321a31fd53ab141d8d48ee426d550aa4c3303ec
anatoly yakovenko <aeyakovenko@gmail.com>
2011-12-20 00:58:25 -0800

    got submit page to add users

--- ServerQ.hs
+++ ServerQ.hs
@@ -1,18 +1,18 @@
 module ServerQ where
 
---import Server
---import qualified Account as Account
---import Data.Acid.Memory                      ( openMemoryState )
+import Server
+import qualified Account as Account
+import Data.Acid.Memory                      ( openMemoryState )
 import Happstack.Lite
 
 test :: IO ()
 test = do
-   --db <- openMemoryState Account.empty
+   db <- openMemoryState Account.empty
    let 
          homePage :: ServerPart Response
          homePage = serveDirectory DisableBrowsing ["index.html"] "frontend/public"
          
-   serve Nothing ( msum [ -- dir "addUser" (addUser db)
-                        homePage
+   serve Nothing ( msum [ dir "addUser" (addUser db)
+                        , homePage
                         ])
 
