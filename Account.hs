d87c0e339793999d335d7b3d334386cb70234f19
anatoly yakovenko <aeyakovenko@gmail.com>
2011-12-19 21:40:38 -0800

    better q framework

--- Account.hs
+++ Account.hs
@@ -1,23 +1,12 @@
 {-# LANGUAGE TypeFamilies, DeriveDataTypeable, TemplateHaskell #-}
 {-# OPTIONS_GHC -fspec-constr-count=2 -fno-warn-orphans #-}
-module Account( addUser
-              , listUsers
-              , loginToCookie 
-              , cookieToUser
-              , clearUserCookie 
-              , AccountError( UserAlreadyExists )
-              , empty
-              , rethrowIO
-              , Database
-              , test
-              )  where
+module Account where
+
 import Control.Monad.IO.Class                ( MonadIO )
 import Data.Word                             ( Word8 )
-import Char                                  ( ord )
 import Monad                                 ( liftM )
 import Control.Monad.State                   ( get, put )
 import Control.Monad.Reader                  ( ask )
-import Data.Acid.Memory                      ( openMemoryState )
 import Control.Monad.Error                   ( Error, runErrorT, throwError, MonadError, liftIO)
 import Data.Typeable                         ()
 import qualified Data.Map                    as Map
@@ -150,49 +139,5 @@ newSalt = liftM B.pack $ sequence $ take 32 $ repeat randomIO
 newCookie :: IO B.ByteString
 newCookie = newSalt
 
-toB :: String -> B.ByteString
-toB ss = B.pack $ map (fromIntegral . ord) ss
 
-test :: IO ()
-test = do
-   let assert msg False = error msg
-       assert _ True = return $ ()
-       isRight (Right _)   = True
-       isRight _           = False
 
-   db <- openMemoryState empty
-
-   _rv <- listUsers db
-   assert "listUsers" (null _rv)
-
-   _rv <- loginToCookie db (toB "hello") (toB "world")
-   assert "login empty" ((Left UserDoesntExist) == _rv)
-
-   _rv <- cookieToUser db (toB "randomnonsense")
-   assert "cookieToUser empty" ((Left BadCookie) == _rv)
-
-   _rv <- addUser db (toB "hello") (toB "world")
-   assert "added user" (_rv == (Right ()))
-
-   _rv <- listUsers db
-   assert "listUsers2" ([toB "hello"] == _rv)
-
-   _rv <- addUser db (toB "hello") (toB "again")
-   assert "added user" (_rv == (Left UserAlreadyExists))
-
-   _rv <- loginToCookie db (toB "hello") (toB "badpassword")
-   assert "login bad password" (_rv == (Left BadPassword))
-
-   _rv <- loginToCookie db (toB "hello") (toB "world")
-   assert "loginToCookie good password" (isRight _rv)
-   let (Right goodcookie) = _rv
-
-   _rv <- cookieToUser db (toB "randomnonsense") 
-   assert "cookieToUser bad cookie" (_rv == (Left BadCookie))
-
-   _rv <- cookieToUser db goodcookie
-   assert "cookieToUser good cookie" (_rv == (Right (toB "hello")))
-
-   clearUserCookie db (toB "hello")
-   _rv <- cookieToUser db goodcookie
-   assert "cookieToUser good cookie" (_rv == (Left BadCookie))
--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 
-out/%_q:%.hs
+out/%Q:%Q.hs
 	@mkdir -p out
-	ghc -hide-package mtl -Wall -Werror -O2 -optl"-Wl,-read_only_relocs,suppress" -outputdir out -main-is $*.test -o $@ --make $*.hs
+	ghc -hide-package mtl -Wall -Werror -O2 -optl"-Wl,-read_only_relocs,suppress" -outputdir out -main-is $*Q.test -o $@ --make $^
 
-out/%.hs.ok:out/%_q
+out/%Q.hs.ok:out/%Q
 	$^ && touch $@
 
-test:$(addsuffix .ok,$(addprefix out/,$(wildcard *.hs)))
+test:$(addsuffix .ok,$(addprefix out/,$(wildcard *Q.hs)))
 
 deps:
 	cabal install acid-state
