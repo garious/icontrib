edb5daed439ada0a85dc0455c5e35dfc6cec990f
Greg Fitzgerald <greg@icontrib.org>
2012-01-06 23:16:28 -0800

    simpilfied module definition syntax.  also made getResources() deterministic so that javascript files may cause side effects, such as setting global variables that the next script will read.  files are interpretted in depth-first order

--- test/nested/nested.js
+++ test/nested/nested.js
@@ -1,7 +1,8 @@
-return YOINK.module(['../assert.js', 'neighbor.js'], function(a, msg) {
-
-    a.assert(msg == 'hello neighbor');
-
-});
+return {
+    deps: ['../assert.js', 'neighbor.js'],
+    callback: function(a, msg) {
+        a.assert(msg == 'hello neighbor');
+    }
+};
 
 
