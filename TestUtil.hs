2c6c43d6072cd0f428e455bc05135df3bc1b7d9b
anatoly yakovenko <aeyakovenko@gmail.com>
2012-01-30 21:54:30 -0800

    site test

--- TestUtil.hs
+++ TestUtil.hs
@@ -9,13 +9,13 @@ toB :: String -> B.ByteString
 toB = B.pack . map (fromIntegral . ord)
 
 -- Assert an ErrorT action returns the expected value
-assertEqErrorT :: (Eq e, Eq a) => String -> ErrorT e IO a -> Either e a -> IO ()
+assertEqErrorT :: (Show e, Show a, Eq e, Eq a) => String -> ErrorT e IO a -> Either e a -> IO ()
 assertEqErrorT msg transaction expected = assertEqM msg (runErrorT transaction) expected
 
 
 -- Assert an ErrorT action returns any value that is Right.  
 -- Return the value inside the Right constructor
-assertRightErrorT :: String -> ErrorT e IO a -> IO a
+assertRightErrorT :: (Show e, Show a) => String -> ErrorT e IO a -> IO a
 assertRightErrorT msg transaction = do
     actual <- runErrorT transaction
     assert msg (isRight actual)
@@ -29,10 +29,10 @@ isRight (Left _)  = False
 
 
 -- Assert a monadic returns the expected value
-assertEqM :: (Eq a) => String -> IO a -> a -> IO ()
+assertEqM :: (Eq a, Show a) => String -> IO a -> a -> IO ()
 assertEqM msg actualM expected = do
     actual <- actualM
-    assert msg (actual == expected)
+    assert (msg ++ " " ++ (show actual)) (actual == expected)
 
 -- Assert the argument is true or bail out with the given error message
 assert :: String -> Bool -> IO ()
