f6988da09777a14cc3bf087dfe402a7445b6a001
Greg Fitzgerald <greg@icontrib.org>
2012-01-31 20:42:20 -0800

    browser can no longer use JsWidget to select a 'main' function.  Instead it routes all params to the client.  Used this feature to remove specific charities from the client.  Also switched from layout.js to layout1.js

--- public/charity/index.js
+++ public/charity/index.js
@@ -1,6 +1,6 @@
 var deps = [
     '/tag/tag.js', 
-    '/tag/layout.js', 
+    '/tag/layout1.js', 
     '/ui/core.js', 
     '/ui/nav.js'
 ];
@@ -12,46 +12,38 @@ function onReady(E, L, CORE, NAV) {
         var user = as.user || defaultUser;
 	var box = CORE.box([
             L.spoon([
-	        CORE.h2(user.firstName),
+	        E.div({style: {height: '30px'}}, [CORE.h2(user.firstName)]),
+                L.pillow(20),
                 L.hug([
+                    E.img({style: {width: '175px', height: '175px', borderRadius: '5px'}, src: user.imageUrl, alt: user.firstName}),
+                    L.pillow(30),
                     L.spoon([
-                        E.img({style: {width: '175px', height: '175px', borderRadius: '5px'}, src: user.imageUrl, alt: user.firstName})
-                    ], 20),
-                    L.spoon([
-                        E.p({style: {width: '600'}}, user.mission),
-                        E.br(),
+                        E.p({style: {height: '100', width: '600'}}, user.mission), 
+                        L.pillow(20),
                         CORE.button({href: '/me/?donateTo=' + user.id}, ['Donate!'])
-                    ], 20)
-                ], 30)
-            ], 20)
+                    ])
+                ])
+            ])
         ]);
 
         return L.spoon([
            L.hug([
-                L.pillow(200,0),
+                L.pillow(220,0),
                 box
-           ], 20),
+           ]),
            L.pillow(30)
         ]);
     }
 
-    function Usoa(params, nodeReady) {
-        require(['usoa.json'], function(u) {
-            nodeReady( NAV.frame([charity({user: u})]) );
-        });
-    }
-
-    function GlobalFundForWomen(params, nodeReady) {
-        require(['gffw.json'], function(u) {
+    function main(params, nodeReady) {
+        require([params.main + '.json'], function(u) {
             nodeReady( NAV.frame([charity({user: u})]) );
         });
     }
 
     define({
         title: 'IContrib.org',
-        main: Usoa,
-        usoa: Usoa,
-        gffw: GlobalFundForWomen
+        main: main
     });
 
 }
