96a8538c950284949c2bfe661aa305e61b84e00e
anatoly yakovenko <aeyakovenko@gmail.com>
2012-02-29 09:30:29 -0800

    missing

---
+++ Data/Popular.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+module Data.Popular where
+
+import Data.Data                             ( Data, Typeable )
+import Data.CharityInfo                      ( CharityID )
+
+data Popular = Popular { cid :: CharityID
+                       , name :: String
+                       }
+             deriving (Eq, Ord, Show, Data, Typeable)
+
+
