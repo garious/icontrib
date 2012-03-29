71e07fa5a31a029ea6fca4ddcf8c18acb06a3d76
anatoly yakovenko <aeyakovenko@gmail.com>
2012-03-29 08:57:30 -0700

    fixed login

--- Client/SignUp.js
+++ Client/SignUp.js
@@ -35,32 +35,7 @@ function onReady(Iface, TwoDim, Tag, Layout, Core, Frame, Colors) {
                 if(data.Left) {
                     badLogin.hidden = false;
                 } else {
-                    // TODO: This should all be automatic after posting to /auth/add
-                    var i = email.value.indexOf('@');
-                    var owner = email.value.substring(0, i > 0 ? i : undefined);
-                    Frame.post('/donor/update', {
-                        owner: owner,
-                        email: email.value,
-
-                        // TODO: This can all be optional
-                        firstName: '',
-                        lastName: '',
-                        phone: '',
-                        imageUrl: '',
-                        centsDonated: 0,
-                        alignedDonated: 0,
-                        alignedUsers: [],
-                        distribution: [],
-                        funds: []
-
-                    }, function(dat) {
-                        var data = JSON.parse(dat);
-                        if(data.Left) {
-                            alert('error: ' + data.Left);
-                        } else {
-                            window.location = '/Me';
-                        }
-                    });
+                    window.location = '/Me';
                 }
             });
         }
