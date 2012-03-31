84e45daae3243952d3fa117602b6b8e92c7e5834
anatoly yakovenko <aeyakovenko@gmail.com>
2012-03-31 16:53:18 -0700

    more paypal stuff, setup a db for balances, make sure i recieve and distribute payments atomically

--- Server/Data/Paypal.hs
+++ Server/Data/Paypal.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving, DeriveDataTypeable, TemplateHaskell #-}
-module Data.UserInfo where
 module Data.Paypal where
 import Data.Time.Format                      ( parseTime )
 import Data.Time.Clock                       ( UTCTime )
@@ -7,19 +6,30 @@ import Text.Email.Validate                   ( EmailAddress, validate )
 import System.Locale                         ( defaultTimeLocale )
 
 newtype Cents = Cents Int
+              deriving Num
 newtype Email = Email EmailAddress
 
-data PaymentStatus = Completed
-                   deriving (Eq, Ord, Show, Data, Typeable)
-data IPN = Payment { reciever_email :: Email
-                   , txn_id         :: String
-                   , payer_email    :: Email
-                   , payment_fee    :: Cents
-                   , payment_gross  :: Cents
-                   , payment_date   :: UTCTime
-                   , payment_status :: PaymentStatus
-                   }
+data IPN = CompletedPayment { reciever_email :: Email
+                            , txn_id         :: String
+                            , payer_email    :: Email
+                            , payment_fee    :: Cents
+                            , payment_gross  :: Cents
+                            , payment_date   :: UTCTime
+                            }
               deriving (Eq, Ord, Show, Data, Typeable)
+
+instance Indexable IPN where
+    empty = ixSet [ ixFun $ \ci -> unzip (urlParams ci)
+                  ]
+$(deriveSafeCopy 0 'base ''IPN)
+
+data IPNMessage = IPNMessage { urlParams :: [(String,String)] }
+                  deriving (Eq, Ord, Show, Data, Typeable)
+
+instance Indexable IPNMessage where
+    empty = ixSet [ ixFun $ \ci -> unzip (urlParams ci)
+                  ]
+$(deriveSafeCopy 0 'base ''IPNMessage)
 
 class Parse a where
     parse :: MonadError String m => String -> m a
@@ -42,4 +52,5 @@ instance Parse Cents where
         when ((floor fl) /= fl) $ throwError $ "float parsing error to cents" ++ str
         return $ Cents (fromRational str) 
 
-
+type IPNMessageDB = IxSet IPNMessage
+type PaymentDB = IxSet Payment
