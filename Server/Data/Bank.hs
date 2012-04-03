e46400e2e167371d53b8bc4de5596da5ba872a10
anatoly yakovenko <aeyakovenko@gmail.com>
2012-04-03 09:00:58 -0700

    reworking how balances are handled

--- Server/Data/Bank.hs
+++ Server/Data/Bank.hs
@@ -3,5 +3,6 @@ module Data.Bank where
 import qualified Data.Login                  as L
 import qualified Data.CharityInfo            as C
 
+data Deposit = Email
 data Ledger = User L.Identity Cents
-            | Charity C.Ein Cents
+            | Charity C.Ein [(L.Identity Cents)]
