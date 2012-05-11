96f705ab3a55a7cf19acfe49fe69acd0a6421db1
Greg Fitzgerald <greg@icontrib.org>
2012-05-10 22:18:54 -0700

    added donor profile page

--- Client/Index.js
+++ Client/Index.js
@@ -1,7 +1,3 @@
-var initialDeps = [
-    '/donor/mostInfluential.json'
-];
-
 var deps = [
     '/donor/checkUser.json',
     '/Tag/Tag.js', 
@@ -9,37 +5,32 @@ var deps = [
     '/Skin/Frame.js', 
     '/Skin/Core.js', 
     '/charity/popular.json', 
-    '/Skin/Donor.js'
+    '/Skin/Donor.js',
+    '/donor/mostInfluentialUser.json'
 ];
 
-function onInitialReady(MostInfluentialId) {
+function onReady(Auth, Tag, Layout, Frame, Core, PopularCharities, Donor, MostInfluential) {
 
-    function onReady(Auth, Tag, Layout, Frame, Core, PopularCharities, Donor, MostInfluential) {
+    var mostInfluential = Core.box({
+        width: 600,
+        contents: Donor.profile1({user: MostInfluential})
+    });
 
-        var mostInfluential = Core.box({
-            width: 600,
-            contents: Donor.profile1({user: MostInfluential})
-        });
-
-        var body = Layout.spoon([
-            Layout.hug([
-                mostInfluential,
-                Layout.pillow(20),
-                Donor.recommendedFunds({funds: PopularCharities})
-            ]),
+    var body = Layout.spoon([
+        Layout.hug([
+            mostInfluential,
             Layout.pillow(20),
-            Frame.footer([
-                Core.hyperlink({url: 'Registration', text: 'Charity Registration'})
-            ])
-        ]);
+            Donor.recommendedFunds({funds: PopularCharities})
+        ]),
+        Layout.pillow(20),
+        Frame.footer([
+            Core.hyperlink({url: 'Registration', text: 'Charity Registration'})
+        ])
+    ]);
 
-        Yoink.define( Frame.frame({contents: body, auth: Auth}) );
+    Yoink.define( Frame.frame({contents: body, auth: Auth}) );
 
-    }
-
-    deps.push('/donor/' + MostInfluentialId + '.json');
-    Yoink.require(deps, onReady);
 }
 
-Yoink.require(initialDeps, onInitialReady);
+Yoink.require(deps, onReady);
 
