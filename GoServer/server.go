a70fc8f8481789e2315eee6cce370c35f39ed57f
Greg Fitzgerald <greg@icontrib.org>
2013-07-28 14:18:03 -0700

    go fmt server.go

--- GoServer/server.go
+++ GoServer/server.go
@@ -1,66 +1,65 @@
 package main
 
 import (
-  "net/http"
-  "net/url"
-  "log"
-  "html/template"
-  "os"
-  "strings"
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 )
 
 func main() {
-  http.HandleFunc("/Yoink.js", onYoink)
-  http.Handle("/Tag/", newJsAppServer("/Tag", "../Tag"))
-  http.Handle("/Toys/", newJsAppServer("/Toys", "../Toys"))
-  http.Handle("/", newJsAppServer("", "../Client"))
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/Yoink.js", onYoink)
+	http.Handle("/Tag/", newJsAppServer("/Tag", "../Tag"))
+	http.Handle("/Toys/", newJsAppServer("/Toys", "../Toys"))
+	http.Handle("/", newJsAppServer("", "../Client"))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 type Page struct {
-  Filename string
-  Params   url.Values
+	Filename string
+	Params   url.Values
 }
 
 type JsAppServer struct {
-  Prefix string
-  Root string
+	Prefix string
+	Root   string
 }
 
 func newJsAppServer(prefix string, root string) http.Handler {
-  return &JsAppServer{Prefix: prefix, Root: root}
+	return &JsAppServer{Prefix: prefix, Root: root}
 }
 
 func (h *JsAppServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  path := h.Root + strings.TrimPrefix(r.URL.Path, h.Prefix)
+	path := h.Root + strings.TrimPrefix(r.URL.Path, h.Prefix)
 
-  if exists(path) {
-    http.ServeFile(w, r, path)
-  } else if exists(path + ".js") {
-    templ := template.New("bar")
-    parsedTempl, _ := templ.Parse(jsAppHtml)
+	if exists(path) {
+		http.ServeFile(w, r, path)
+	} else if exists(path + ".js") {
+		templ := template.New("bar")
+		parsedTempl, _ := templ.Parse(jsAppHtml)
 
-    r.ParseForm() // Parse the parameters in the request's URI
-    page := &Page{Filename: r.URL.Path + ".js", Params: r.Form}
+		r.ParseForm() // Parse the parameters in the request's URI
+		page := &Page{Filename: r.URL.Path + ".js", Params: r.Form}
 
-    w.Header().Set("Content-Type", "text/html")
-    parsedTempl.Execute(w, page)
-  } else {
-    http.NotFound(w, r)
-  }
+		w.Header().Set("Content-Type", "text/html")
+		parsedTempl.Execute(w, page)
+	} else {
+		http.NotFound(w, r)
+	}
 }
 
 func onYoink(w http.ResponseWriter, r *http.Request) {
-  http.ServeFile(w, r, "../Yoink/Yoink.js")
+	http.ServeFile(w, r, "../Yoink/Yoink.js")
 }
 
 func exists(path string) bool {
-  _, err := os.Stat(path)
-  return err == nil
+	_, err := os.Stat(path)
+	return err == nil
 }
 
-var jsAppHtml =
-`<!DOCTYPE html>
+var jsAppHtml = `<!DOCTYPE html>
 <html>
   <head></head>
   <body>
