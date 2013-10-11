71b1cd2d27f737ee582663c49be62ab532a8a1e2
Greg Fitzgerald <greg@icontrib.org>
2013-10-11 14:45:43 -0700

    moved go server to top-level directory

--- icontrib.go
+++ icontrib.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-	jsappserver.HandleDir("/Tag/", "../Tag")
-	jsappserver.HandleDir("/Toys/", "../Toys")
-	jsappserver.HandleDir("/", "../Client")
+	jsappserver.HandleDir("/Tag/", "./Tag")
+	jsappserver.HandleDir("/Toys/", "./Toys")
+	jsappserver.HandleDir("/", "./Client")
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
