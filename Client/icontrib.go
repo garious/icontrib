b343c5db9c23f3e56372aac88836d1072cce9b6b
Greg Fitzgerald <greg@icontrib.org>
2013-10-15 22:55:38 -0700

    slightly better server data

--- Client/icontrib.go
+++ Client/icontrib.go
@@ -37,18 +37,64 @@ func popular(w http.ResponseWriter, r *http.Request) {
 }
 
 func community(w http.ResponseWriter, r *http.Request) {
-	b, err := ioutil.ReadFile("../private/static/donor/greg.json")
+	userBlob, err := ioutil.ReadFile("../private/static/donor/greg.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var userInfo UserInfo
+
+	err = json.Unmarshal(userBlob, &userInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err := json.Marshal(userInfo)
 	if err != nil {
 		log.Fatal(err)
 	}
 	w.Write(b)
 }
 
+type UserInfo struct {
+	Owner          string         `json:"owner"`
+	FirstName      string         `json:"firstName"`
+	LastName       string         `json:"lastName"`
+	Phone          string         `json:"phone"`
+	Email          string         `json:"email"`
+	ImageUrl       string         `json:"imageUrl"`
+	CentsDonated   int            `json:"centsDonated"`
+	AlignedDonated int            `json:"alignedDonated"`
+	AlignedUsers   []string       `json:"alignedUsers"`
+	Distribution   []Distribution `json:"distribution"`
+	Funds          []Fund         `json:"funds"`
+}
+
+type Distribution struct {
+	Name   string   `json:"name"`
+	Cid    string   `json:"cid"`
+	Shares float64  `json:"shares"`
+	Labels []string `json:"labels"`
+}
+
+type Fund struct {
+	Name  string `json:"name"`
+	Label string `json:"label"`
+}
+
 func checkUser(w http.ResponseWriter, r *http.Request) {
 	type Auth struct {
-		Right string
+		Right UserInfo
 	}
-	b, err := json.Marshal(Auth{"greg"})
+	userBlob, err := ioutil.ReadFile("../private/static/donor/greg.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var userInfo UserInfo
+
+	err = json.Unmarshal(userBlob, &userInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err := json.Marshal(Auth{userInfo})
 	if err != nil {
 		log.Fatal(err)
 	}
