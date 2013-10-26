ad5eb6555279ea7af3551237dfa1e68a97052947
Greg Fitzgerald <greg@icontrib.org>
2013-10-25 18:56:32 -0700

    use the yoink go module in sample server

--- yoink.go
+++ yoink.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"github.com/garious/yoink/jsappserver"
+	"github.com/garious/yoink/yoink"
 	"log"
 	"net/http"
-	"path"
-	"runtime"
 )
 
 func main() {
-	_, filename, _, _ := runtime.Caller(0)
-	yoinkDir := path.Join(path.Dir(filename), "yoink")
-	jsappserver.HandleDir("/yoink/", yoinkDir)
+	jsappserver.HandleDir("/yoink/", yoink.Dir())
 	jsappserver.HandleDir("/", ".")
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
