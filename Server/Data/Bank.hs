8de0e8bcfb7023025c0ba12d29479ac64df0eb13
Greg Fitzgerald <greg@icontrib.org>
2014-03-05 17:14:15 -0800

    Saying goodbye to the Haskell server

--- Server/Data/Bank.hs
+++
@@ -1,8 +0,0 @@
-module Data.Bank where
-
-import qualified Data.Login                  as L
-import qualified Data.CharityInfo            as C
-
-data Deposit = Email
-data Ledger = User L.Identity Cents
-            | Charity C.Ein [(L.Identity Cents)]
