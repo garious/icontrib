de83b2f5be0c1253aba101df03f8e9656aa1ccfc
Greg Fitzgerald <greg@icontrib.org>
2014-04-22 23:37:47 -0700

    examples no longer use the yoink.Dir() trick, which doesn't work in AppEngine

--- examples/canvas/server.go
+++ examples/canvas/server.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"github.com/garious/yoink/jsappserver"
-	"github.com/garious/yoink/yoink"
 	"log"
 	"net/http"
 )
 
 func main() {
-	jsappserver.HandleDir("/yoink/", yoink.Dir())
+	jsappserver.HandleDir("/yoink/", "../../stdlib")
 	jsappserver.HandleDir("/", ".")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- examples/hello/server.go
+++ examples/hello/server.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"github.com/garious/yoink/jsappserver"
-	"github.com/garious/yoink/yoink"
 	"log"
 	"net/http"
 )
 
 func main() {
-	jsappserver.HandleDir("/yoink/", yoink.Dir())
 	jsappserver.HandleDir("/", ".")
-
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
